e1367ef94b37c7dd4b43a5d4d469bec164b326de
thepartyneverends <ustimenkosergey05@yandex.ru>
2025-01-13 15:16:31 +0300

    133

---
+++ scripts/wow.js
@@ -0,0 +1,12 @@
+var wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animate__animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true,        // default
+    callback: function(box){},
+    scrollContainer: null
+  }
+  )
+wow.init();
